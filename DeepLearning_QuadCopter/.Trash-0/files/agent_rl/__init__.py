e6553f6718f85cb6946aaa362d1d91aa9daa90d8
kanjasaha <kanjasaha@gmail.com>
2019-12-06 21:13:05 -0800

    a few more notebooks

--- DeepLearning_QuadCopter/.Trash-0/files/agent_rl/__init__.py
+++
@@ -1,2 +0,0 @@
-from agent_rl.base_agent import BaseAgent
-from agent_rl.policy_search import RandomPolicySearch
--- DeepLearning_QuadCopter/.Trash-0/files/task_rl/__init__.py
+++
@@ -1,2 +0,0 @@
-from task_rl.base_task import BaseTask
-from task_rl.takeoff import Takeoff
